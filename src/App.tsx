44d4ae7293f46f3e6babcf073cabc95a1d00b25f
molinax18 <arielmolina8953@gmail.com>
2026-08-19 19:21:37 -0300

    feat: implement basic html structure in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import HeroImage from "./components/HeroImage";
+import JobListingContainer from "./components/JobListingContainer";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="grid grid-rows-[min-content_1fr] gap-y-6 min-h-dvh">
+      <HeroImage />
+      <main>
+        <JobListingContainer />
+      </main>
+    </div>
+  );
 }
