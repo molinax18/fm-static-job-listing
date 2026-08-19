c5aa600304178f41d6779214886507042bf2692c
molinax18 <arielmolina8953@gmail.com>
2026-08-19 19:21:08 -0300

    feat: implement new HeroImage component

---
+++ src/components/HeroImage.tsx
@@ -0,0 +1,10 @@
+export default function HeroImage() {
+  return (
+    <div
+      className="
+      bg-[url(./assets/bg-header-mobile.svg)]
+      lg:bg-[url(./assets/bg-header-desktop.svg)]
+      bg-center bg-green-400 h-36"
+    ></div>
+  );
+}
